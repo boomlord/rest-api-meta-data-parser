7cbc9dcd48f724d91a1dd63e79a3b440744e8fd8
Nguyen Pham <npham22@csc.com>
2017-12-15 12:52:20 +0700

    Update

--- api/routes/parserRoute.js
+++ api/routes/parserRoute.js
@@ -2,6 +2,10 @@
 module.exports = function(app) {
   var parser = require('../controllers/parserController');
 
+  app.route('/', function (req, res) {
+  	res.send('Meta Data Parser RestAPI')
+  })
+
   app.route('/parser/:landingUrl')
     .get(parser.parse_landingurl);
 };
